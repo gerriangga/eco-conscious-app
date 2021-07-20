9904b7481d31369c185dbb341d78030e49f70b21
Matius Rimbe <matiusrimbe.ptik@qmail.id>
2021-07-20 22:33:57 +0800

    [UPDATE] create PurchaseProductController

--- src/main/java/com/example/EcoConsciousApp/constant/ApiUrlConstant.java
+++ src/main/java/com/example/EcoConsciousApp/constant/ApiUrlConstant.java
@@ -3,4 +3,5 @@ package com.example.EcoConsciousApp.constant;
 public class ApiUrlConstant {
     public static final String CUSTOMER = "/api/customers";
     public static final String USABLE_PRODUCT = "/api/usable-products";
+    public static final String PURCHASE_PRODUCT = "/api/purchase-products";
 }
---
+++ src/main/java/com/example/EcoConsciousApp/controller/PurchaseProductController.java
@@ -0,0 +1,36 @@
+package com.example.EcoConsciousApp.controller;
+
+import com.example.EcoConsciousApp.constant.ApiUrlConstant;
+import com.example.EcoConsciousApp.constant.ResponseMessage;
+import com.example.EcoConsciousApp.entity.PurchaseProduct;
+import com.example.EcoConsciousApp.service.PurchaseProductService;
+import com.example.EcoConsciousApp.utils.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(ApiUrlConstant.PURCHASE_PRODUCT)
+public class PurchaseProductController {
+    @Autowired
+    PurchaseProductService purchaseProductService;
+
+    @PostMapping
+    public ResponseEntity<Response<PurchaseProduct>> customerPurchaseProduct(@RequestBody PurchaseProduct purchaseProduct){
+        String message = String.format(ResponseMessage.DATA_INSERTED, "purchase product");
+        Response<PurchaseProduct> response = new Response<>();
+        response.setMessage(message);
+        response.setData(purchaseProductService.transaction(purchaseProduct));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response);
+    }
+
+    @GetMapping("/{id}")
+    public PurchaseProduct getPurchaseProduct(@PathVariable  String id){
+        return purchaseProductService.getTransactionById(id);
+    }
+
+}
